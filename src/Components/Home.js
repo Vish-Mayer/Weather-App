c49e3a9d0c0e7ebda90d9c639400f2f1fb595c15
Vish Mayer <vish.mayer@gmail.com>
2021-04-01 09:48:59 +0100

    imported graphql query

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,11 +3,20 @@ import { useLazyQuery } from '@apollo/client'
 import { GET_WEATHER_QUERY } from '../Graphql/Queries'
 
 function Home() {
+
+  const [getWeather, {loading, data, error}] = useLazyQuery(GET_WEATHER_QUERY, {
+    variables: { name: "New York"},
+  })
+
+  if (error) return <h1>Error Found</h1>
+  if (data) {
+    console.log(data)
+  }
   return (
     <div className="home">
       <h1>Search Weather</h1>
       <input id="text-input" type="text" placeholder="City name..."/>
-      <button id="search">Search</button>
+      <button onClick={ () => getWeather()} id="search">Search</button>
     </div>
   );
 }
