e3a2f362e9b1d42a2ba302012fbc4dec45eeb2d7
Vish Mayer <vish.mayer@gmail.com>
2021-04-01 10:02:58 +0100

    removing hard coded search

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,11 +1,14 @@
 
-import { useLazyQuery } from '@apollo/client'
-import { GET_WEATHER_QUERY } from '../Graphql/Queries'
+import React, { useState } from 'react';
+import { useLazyQuery } from '@apollo/client';
+import { GET_WEATHER_QUERY } from '../Graphql/Queries';
 
 function Home() {
 
+  const [citySearched, setCitySearched] = useState("")
+
   const [getWeather, {loading, data, error}] = useLazyQuery(GET_WEATHER_QUERY, {
-    variables: { name: "New York"},
+    variables: { name: citySearched }
   })
 
   if (error) return <h1>Error Found</h1>
@@ -15,7 +18,14 @@ function Home() {
   return (
     <div className="home">
       <h1>Search Weather</h1>
-      <input id="text-input" type="text" placeholder="City name..."/>
+      <input
+        id="text-input"
+        type="text"
+        placeholder="City name..."
+        onChange={(event) => {
+          setCitySearched(event.target.value);
+        }}
+      />
       <button onClick={ () => getWeather()} id="search">Search</button>
     </div>
   );
