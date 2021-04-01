fedf7a18f55a26701129a7496aa5fa6c1fd8c161
Vish Mayer <vish.mayer@gmail.com>
2021-04-01 09:08:06 +0100

    basic app controllers

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,6 +3,8 @@ function Home() {
   return (
     <div className="home">
       <h1>Search Weather</h1>
+      <input id="text-input" type="text" placeholder="City name..."/>
+      <button id="search">Search</button>
     </div>
   );
 }
--- src/Components/Home.test.js
+++ src/Components/Home.test.js
@@ -17,4 +17,13 @@ describe('Home', () => {
   it('renders title of application', () => {
     expect(wrapper.find("h1").text()).toContain("Search Weather")
   });
+
+  it('renders an empty text field with a user prompt', () => {
+  expect(wrapper.find('#text-input').at(0).props().placeholder).toEqual('City name...')
+  });
+
+  it('renders a button with text "Search"', () => {
+  expect(wrapper.find("#search").text()).toBe("Search")
+  })
+
 });
